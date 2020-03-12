ddd735587bfa9705d2b4c3f8da447c0f1d05fc67
karenbennis <bennis.karen@gmail.com>
2020-03-12 12:00:53 -0400

    clean up files, move all practice into one file

--- Python_practice.py
+++ Python_practice.py
@@ -21,7 +21,44 @@ if "Arapahoe" in counties or "El Paso" in counties:
     print("Arapahoe or El Paso is in the list of counties.")
 else:
     print("Arapahoe and El Paso are not in the list of counties.")
-    
+
+# Practice while loop
+x = 0
+while x <= 5:
+    print(x)
+    x = x + 1
+
+# Practice for loop
+numbers = [0,1,2,3,4]
+for num in numbers:
+    print(num)
+
+for num in range(5):
+    print(num)
+
+# Practice simple if else
+temperature = int(input("What is the temperature outside?"))
+if temperature >80:
+    print("Turn on the AC.")
+else:
+    print("Open the windows.")
+
+# Practice if-elif-else
+# What is the score?
+score=int(input("What is your test score?"))
+
+# Determine the grade.
+if score >=90:
+    print("Your grade is an A.")
+elif score >=80:
+    print("Your grade is a B.")
+elif score >=70:
+    print("Your grade is a C.")
+elif score >=60:
+    print("Your grade is a D.")
+else:
+    print("Your grade is an F.")
+
 for county in counties:
     print(county)
 
--- practice_for_loops.py
+++
@@ -1,6 +0,0 @@
-numbers = [0,1,2,3,4]
-for num in numbers:
-    print(num)
-
-for num in range(5):
-    print(num)
